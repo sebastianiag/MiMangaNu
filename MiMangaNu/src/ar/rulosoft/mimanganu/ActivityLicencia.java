aa23e41161bb7e5cb5063f8f19e801a562d2e775
raul <raul@rulosoft>
2014-12-23 09:31:19 -0300

    Fix

--- MiMangaNu/src/ar/rulosoft/mimanganu/ActivityLicencia.java
+++ MiMangaNu/src/ar/rulosoft/mimanganu/ActivityLicencia.java
@@ -2,12 +2,14 @@ package ar.rulosoft.mimanganu;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import ar.rulosoft.mimanganu.R;
+
 
 public class ActivityLicencia extends ActionBarActivity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		setContentView(R.layout.activity_licence);
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_licence);
 	}
 
 }

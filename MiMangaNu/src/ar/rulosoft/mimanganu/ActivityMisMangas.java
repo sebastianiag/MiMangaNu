df0dde36887c35968fd200c1412189e842f9cfe2
raul <raul@rulosoft>
2015-01-09 08:51:11 -0300

    cambios en el manejo de dialogos

--- MiMangaNu/src/ar/rulosoft/mimanganu/ActivityMisMangas.java
+++ MiMangaNu/src/ar/rulosoft/mimanganu/ActivityMisMangas.java
@@ -9,28 +9,22 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.preference.PreferenceManager;
-import android.provider.Settings;
 import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.Surface;
 import android.view.View;
 import android.view.ContextMenu.ContextMenuInfo;
-import android.view.WindowManager;
 import ar.rulosoft.mimanganu.componentes.Database;
 import ar.rulosoft.mimanganu.componentes.Manga;
 import ar.rulosoft.mimanganu.servers.ServerBase;
@@ -92,7 +86,7 @@ public class ActivityMisMangas extends ActionBarActivity {
 
 		int id = item.getItemId();
 		if (id == R.id.buscarActualizaciones) {
-			new BuscarNuevo().execute();
+			new BuscarNuevo().setActivity(ActivityMisMangas.this).execute();
 			return true;
 		} else if (id == R.id.action_mostrar_en_galeria) {
 			File f = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/MiMangaNu/", ".nomedia");
@@ -126,40 +120,6 @@ public class ActivityMisMangas extends ActionBarActivity {
 			mViewPager.setCurrentItem((ci - 1));
 		else
 			super.onBackPressed();
-	}
-
-	@SuppressLint("InlinedApi")
-	public static boolean lockOrintation(Activity activity) {
-		boolean locked = false;
-
-		if (Settings.System.getInt(activity.getContentResolver(), Settings.System.ACCELEROMETER_ROTATION, 1) != 1) {
-			locked = true;
-		}
-
-		if (!locked) {
-			int orientation = activity.getRequestedOrientation();
-			int rotation = ((WindowManager) activity.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getRotation();
-			if (android.os.Build.VERSION.SDK_INT > 8) {
-				switch (rotation) {
-				case Surface.ROTATION_0:
-					orientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
-					break;
-				case Surface.ROTATION_90:
-					orientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
-					break;
-				case Surface.ROTATION_180:
-					orientation = ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT;
-					break;
-				default:
-					orientation = ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE;
-					break;
-				}
-				activity.setRequestedOrientation(orientation);
-			} else {
-				activity.setRequestedOrientation(activity.getResources().getConfiguration().orientation);
-			}
-		}
-		return locked;
 	}
 
 	public class SectionsPagerAdapter extends FragmentPagerAdapter {
@@ -201,15 +161,38 @@ public class ActivityMisMangas extends ActionBarActivity {
 		}
 	}
 
-	public class BuscarNuevo extends AsyncTask<Void, String, Void> {
+	public static class BuscarNuevo extends AsyncTask<Void, String, Void> {
+		Activity activity;
+		ProgressDialog progreso;
+		static boolean running = false;
+		static BuscarNuevo actual = null;
+		String msg;
+		
+		public BuscarNuevo setActivity(Activity activity) {
+			this.activity = activity;
+			return this;
+		}
 
-		ProgressDialog progreso = new ProgressDialog(ActivityMisMangas.this);
-		boolean locked = false;
+		public static void onActivityPaused() {
+			if (running && actual.progreso != null)
+				actual.progreso.dismiss();
+		}
+
+		public static void onActivityResumed(Activity actvt) {
+			if (running && actual != null) {
+				actual.progreso = new ProgressDialog(actvt);
+				actual.progreso.setMessage(actual.msg);
+				actual.progreso.show();
+			}
+		}
 
 		@Override
 		protected void onPreExecute() {
-			locked = lockOrintation(ActivityMisMangas.this);
-			progreso.setTitle(getResources().getString(R.string.buscandonuevo));
+			running = true;
+			actual = this;
+			progreso = new ProgressDialog(activity);
+			msg = activity.getResources().getString(R.string.buscandonuevo);
+			progreso.setTitle(msg);
 			progreso.show();
 			super.onPreExecute();
 		}
@@ -217,7 +200,8 @@ public class ActivityMisMangas extends ActionBarActivity {
 		@Override
 		protected void onProgressUpdate(String... values) {
 			final String s = values[0];
-			runOnUiThread(new Runnable() {
+			msg = s;
+			activity.runOnUiThread(new Runnable() {
 				@Override
 				public void run() {
 					progreso.setMessage(s);
@@ -228,30 +212,29 @@ public class ActivityMisMangas extends ActionBarActivity {
 
 		@Override
 		protected Void doInBackground(Void... params) {
-			ArrayList<Manga> mangas = Database.getMangasForUpdates(ActivityMisMangas.this);
-			Database.removerCapitulosHuerfanos(ActivityMisMangas.this);
+			ArrayList<Manga> mangas = Database.getMangasForUpdates(activity);
+			Database.removerCapitulosHuerfanos(activity);
 			for (int i = 0; i < mangas.size(); i++) {
 				Manga manga = mangas.get(i);
 				ServerBase s = ServerBase.getServer(manga.getServerId());
 				try {
 					onProgressUpdate(manga.getTitulo());
 					s.cargarCapitulos(manga);
-					int diff = s.buscarNuevosCapitulos(manga, ActivityMisMangas.this);
-					if (0 < diff && fragmentMisMangas.adapter != null) {
-						fragmentMisMangas.adapter.getItem(i).setNuevos(diff);
+					int diff = s.buscarNuevosCapitulos(manga, activity);
+					if (0 < diff && ((ActivityMisMangas) activity).fragmentMisMangas.adapter != null) {
+						((ActivityMisMangas) activity).fragmentMisMangas.adapter.getItem(i).setNuevos(diff);
 					}
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 			return null;
-
 		}
 
 		@Override
 		protected void onPostExecute(Void result) {
-			if (fragmentMisMangas.adapter != null)
-				fragmentMisMangas.adapter.notifyDataSetChanged();
+			if (((ActivityMisMangas) activity).fragmentMisMangas.adapter != null)
+				((ActivityMisMangas) activity).fragmentMisMangas.adapter.notifyDataSetChanged();
 			if (progreso != null && progreso.isShowing()) {
 				try {
 					progreso.dismiss();
@@ -259,11 +242,21 @@ public class ActivityMisMangas extends ActionBarActivity {
 
 				}
 			}
-			if (!locked)
-				setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
-			//super.onPostExecute(result);
+			running = false;
+			actual = null;
 		}
+	}
 
+	@Override
+	protected void onPause() {
+		BuscarNuevo.onActivityPaused();
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		BuscarNuevo.onActivityResumed(ActivityMisMangas.this);
+		super.onResume();
 	}
 
 }
